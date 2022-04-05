b55f7fea3961615f819a7cfe6d6fe81a336e5593
Jantu Deb <jantud54@gmail.com>
2022-04-05 22:14:02 +0530

    added deploy url for cors

--- config/cors-config.js
+++ config/cors-config.js
@@ -2,7 +2,7 @@ const corsConFig = {
   origin: [
     "http://localhost:3000",
     "https://ionvu-live.netlify.app",
-    "https://deploy-preview-12--ionvu-live.netlify.app",
+    "https://deploy-preview-13--ionvu-live.netlify.app",
   ],
   credentials: true,
 };
