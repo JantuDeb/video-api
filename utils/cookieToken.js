4093814f7ebd929a86d04ec73b0414ef18b34c8a
Jantu Deb <63020886+JantuDeb@users.noreply.github.com>
2022-05-11 01:56:00 +0530

    Updated cookie ecpiry

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -1,7 +1,7 @@
 const cookieToken = (user) => {
   const token = user.getJwtToken();
   const options = {
-    expires: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000),
+    expires: new Date(Date.now() + 30* 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: true,
     sameSite: "none",
